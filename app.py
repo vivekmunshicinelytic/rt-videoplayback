df03d94b1642f5ae00d0fbeac1105e9e5a18fe5d
vivekmunshicinelytic <vivekmunshi@cinelytic.com>
2025-03-04 10:27:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,24 +26,24 @@ def get_weather(api_key, city):
     except Exception as e:
         return None
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    data = request.get_json(silent=True) or {}
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+    # data = request.get_json(silent=True) or {}
     
-    city = request.form.get("city", "London")
-    api_key = os.getenv("WEATHER_API_KEY", "62c5ae06c3c40af76390a238bb76c7dd")
+    # city = request.form.get("city", "London")
+    # api_key = os.getenv("WEATHER_API_KEY", "62c5ae06c3c40af76390a238bb76c7dd")
     
     # if request.method == 'POST':
         # city = request.form['city']
         # api_key = var.key
     
-    transcode_data = {
-        "SOURCE_FILE_NAME": data.get("SOURCE_FILE_NAME", os.getenv("SOURCE_FILE_NAME", "Not Set")),
-        "SourceBucketName": data.get("SourceBucketName", os.getenv("SourceBucketName", "Not Set")),
-        "DestBucketName": data.get("DestBucketName", os.getenv("DestBucketName", "Not Set"))
-    }
-    weather_data = get_weather(api_key, city)
-    return render_template('index.html', weather_data=weather_data, transcode_data=transcode_data)
+    # transcode_data = {
+      #  "SOURCE_FILE_NAME": data.get("SOURCE_FILE_NAME", os.getenv("SOURCE_FILE_NAME", "Not Set")),
+      #  "SourceBucketName": data.get("SourceBucketName", os.getenv("SourceBucketName", "Not Set")),
+      #  "DestBucketName": data.get("DestBucketName", os.getenv("DestBucketName", "Not Set"))
+    # }
+    #weather_data = get_weather(api_key, city)
+    # return render_template('index.html', weather_data=weather_data, transcode_data=transcode_data)
 
 if __name__ == "__main__":
     #app.run(debug=True,port=8080)
